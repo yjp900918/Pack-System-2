36f1833052e2bb5c3a87cd60fd5722a2104a8efd
yjp900918 <yjp900918@gmail.com>
2018-04-01 11:56:14 -0400

    pack-system 2

---
+++ Assets/ItemData.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemData : MonoBehaviour,IBeginDragHandler,IDragHandler,IEndDragHandler {
+
+    public Item item;
+    public int slotIndex;
+
+    Bag bag;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if(item != null)
+        {
+            this.transform.SetParent(transform.parent.parent);
+            this.transform.position = eventData.position;
+            GetComponent<CanvasGroup>().blocksRaycasts = false;
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if(item != null)
+        {
+            this.transform.position = eventData.position;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        this.transform.SetParent(bag.slots[slotIndex].transform);
+        this.transform.position = this.transform.parent.position;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+
+    // Use this for initialization
+    void Start () {
+        bag = GameObject.Find("ItemDataBase").GetComponent<Bag>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Slot.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Slot : MonoBehaviour, IDropHandler {
+
+    public int slotID;
+
+    Bag bag;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        ItemData droppedItem = eventData.pointerDrag.GetComponent<ItemData>();
+        if(bag.items[slotID].ID == -1)
+        {
+            bag.items[droppedItem.slotIndex] = new Item();
+            droppedItem.slotIndex = slotID;
+            bag.items[slotID] = droppedItem.item;
+        }
+        else if(droppedItem.slotIndex != slotID)
+        {
+            Transform item = this.transform.GetChild(0);
+            item.GetComponent<ItemData>().slotIndex = droppedItem.slotIndex;
+            item.transform.SetParent(bag.slots[droppedItem.slotIndex].transform);
+            item.transform.position = item.transform.parent.position;
+
+            bag.items[droppedItem.slotIndex] = item.GetComponent<ItemData>().item;
+            droppedItem.slotIndex = slotID;
+            bag.items[slotID] = droppedItem.item;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        bag = GameObject.Find("ItemDataBase").GetComponent<Bag>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
